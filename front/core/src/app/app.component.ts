935cd9287d33c9fd101f267a020bc639e03ac403
Kohei Sugimura <sugimuracraft@gmail.com>
2025-03-01 23:06:45 +0900

    upgrade to v18 from v17.

--- front/core/src/app/app.component.ts
+++ front/core/src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ HttpClientModule, RouterOutlet ],
+  imports: [ RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
